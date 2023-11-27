e346328711cc293a5bf45a3e56f75c2bba31a201
hungpv <phanhung2702@gmail.com>
2023-11-27 16:17:57 +0700

    [Add] nav props with type error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,22 @@
-function Navbar() {
+import './index.css'
+
+interface IMenuItem {
+  menuNo: number;
+  // menuName: string;
+  // displayYn: string;
+  // menuPath: string;
+  // sortNo: number;
+  // readYn: string;
+  // useYn: string;
+  // writeYn: string;
+  // parentMenuNo?: number | null;
+}
+
+
+function Navbar(menu: IMenuItem[]) {
   return (
-    <div className="App">
+    <div className="navbar">
+      navbar
     </div>
   );
 }
