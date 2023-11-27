c98523a83ee13472a981f0684c357942cb8dde40
hungpv <phanhung2702@gmail.com>
2023-11-27 16:56:57 +0700

    [Fix] nav with param type now work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,25 @@ import './index.css'
 
 interface IMenuItem {
   menuNo: number;
-  // menuName: string;
-  // displayYn: string;
-  // menuPath: string;
-  // sortNo: number;
-  // readYn: string;
-  // useYn: string;
-  // writeYn: string;
-  // parentMenuNo?: number | null;
+  menuName: string;
+  displayYn: string;
+  menuPath: string;
+  sortNo: number;
+  readYn: string;
+  useYn: string;
+  writeYn: string;
+  parentMenuNo?: number | null;
+}
+
+interface IParams {
+  menu: IMenuItem[];
 }
 
 
-function Navbar(menu: IMenuItem[]) {
+function Navbar(params: IParams) {
+  const { menu } = params
   return (
     <div className="navbar">
-      navbar
     </div>
   );
 }
